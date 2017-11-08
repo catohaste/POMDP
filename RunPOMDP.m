2cd738e5da0c60984ebdcd0a750d2040f3d40965
heykayhay <heykayhay@users.noreply.github.com>
2017-11-08 12:02:30 +0000

    Add in structures into RunPOMPD.

--- RunPOMDP.m
+++ RunPOMDP.m
@@ -6,14 +6,8 @@ alpha    = params(1);
 rho      = params(2);
 noiseSTD = params(3);
 
-stimTrials = input(:,1);
-
-
-% outcomes of correct trials:
-
-%         blk1     blk2
-reward = [1+rho,   1      ; % L action
-          1    ,   1+rho] ; % R action
+stimTrials = input.stimTrials;
+extraReward = input.extraRewardTrials;
 
 
 % set run numbers
@@ -22,120 +16,153 @@ trialN = length(stimTrials);
 
 
 % initialise variables, for speed
-action = nan(trialN,iterN);
-QL = nan (trialN,iterN);
-QR = nan (trialN,iterN);
-delta = nan (trialN,iterN);
+action	= cell(trialN,iterN);
+QL			= zeros(trialN,iterN);
+QR			= zeros(trialN,iterN);
+delta		= zeros(trialN,iterN);
 
- 
+
 for iter = 1:iterN
-   
-   % initalise Q values for each iteration
-   QLL(1,:) = 1;
-   QRR(1,:) = 1;
-   QLR(1,:) = 0;
-   QRL(1,:) = 0;
-   
-   % start model
-   for trials = 1 : trialN
-      
-
-      % set contrast
-      currentStim = stimTrials(trials);
-      
-      % add sensory noise
-      stim_withnoise  = currentStim + noiseSTD * randn;
-
-      
-      % calculate belief
-      Belief_L = normcdf(0,stim_withnoise,noiseSTD);
-      Belief_R = 1 - Belief_L;
-      
-      
-      %initialise Q values for this iteration
-      QL(trials,iter) = Belief_L*QLL + Belief_R*QRL  ;
-      QR(trials,iter) = Belief_L*QLR + Belief_R*QRR  ;
-      
-      
-      % action <-- max(QL,QR)
-      if QL(trials,iter) > QR(trials,iter)
-         
-         action(trials,iter) = -1;
-         
-      elseif QL(trials,iter) < QR(trials,iter)
-         
-         action(trials,iter) = 1;
-         
-      else
-         
-         if rand > 0.5
-            action(trials,iter) = 1;
-         else
-            action(trials,iter) = -1; 
-         end
-         
-      end
-
-
-      % trial reward for action chosen by agent
-      if currentStim<0 && action(trials,iter)==-1
-         
-         Reward = reward(1,input(trials,2));
-         
-      elseif currentStim>0 && action(trials,iter)==1
-         
-         Reward = reward(2,input(trials,2));
-         
-      elseif currentStim==0
-         
-         if rand > 0.5
-            
-            if action(trials,iter)==-1
-               
-               Reward = reward(1,input(trials,2));
-               
-            elseif action(trials,iter)==1
-               
-               Reward = reward(2,input(trials,2));
-               
-            end
-            
-         else
-            
-            Reward = 0 ;
-            
-         end
-         
-      else
-         
-         Reward = 0 ;
-         
-      end
-      
-      
-      % calculate delta, and update Q values
-      if action(trials,iter) == -1  % left
-         
-         delta(trials, iter)   = Reward - QL(trials,iter);
-         
-         QLL     = QLL + alpha * delta(trials,iter) * Belief_L ;
-         QRL     = QRL + alpha * delta(trials,iter) * Belief_R ;
-         
-      else   % right
-         
-         delta(trials, iter)   = Reward - QR(trials,iter);
-         
-         QLR     = QLR + alpha * delta(trials,iter) * Belief_L;
-         QRR     = QRR + alpha * delta(trials,iter) * Belief_R;
-         
-      end
-      
-   end
-   
+	
+	% initalise Q values for each iteration
+	QLL(1,:) = 1;
+	QRR(1,:) = 1;
+	QLR(1,:) = 0;
+	QRL(1,:) = 0;
+	
+	% start model
+	for trial=1:trialN
+		
+		
+		% set contrast
+		currentStim = stimTrials(trial);
+		
+		% add sensory noise
+		stim_withnoise  = currentStim + noiseSTD * randn;
+		
+		
+		% calculate belief
+		Belief_L = normcdf(0,stim_withnoise,noiseSTD);
+		Belief_R = 1 - Belief_L;
+		
+		
+		%initialise Q values for this iteration
+		QL(trial,iter) = Belief_L*QLL + Belief_R*QRL  ;
+		QR(trial,iter) = Belief_L*QLR + Belief_R*QRR  ;
+		
+		
+		% action <-- max(QL,QR)
+		if QL(trial,iter) > QR(trial,iter)
+			
+			action{trial,iter} = 'left';
+			
+		elseif QL(trial,iter) < QR(trial,iter)
+			
+			action{trial,iter} = 'right';
+			
+		else
+			
+			if rand >= 0.5
+				action{trial,iter} = 'right';
+			else
+				action{trial,iter} = 'left';
+			end
+			
+		end
+		
+		% trial reward for action chosen by agent
+		if currentStim<0 && strcmp(action{trial,iterN},'left')
+			
+			switch extraReward{trial}
+				case 'left'
+					reward = 1 + rho;
+				case 'right'
+					reward = 1;
+				case 'none'
+					reward = 1;
+			end
+			
+		elseif currentStim>0 && strcmp(action{trial,iter},'right')
+			
+			switch extraReward{trial}
+				case 'left'
+					reward = 1;
+				case 'right'
+					reward = 1 + rho;
+				case 'none'
+					reward = 1;
+			end
+			
+		elseif currentStim==0
+			
+			if rand > 0.5
+				
+				if strcmp(action{trial,iterN},'left')
+					
+					switch extraReward{trial}
+						case 'left'
+							reward = 1 + rho;
+						case 'right'
+							reward = 1;
+						case 'none'
+							reward = 1;
+					end
+					
+				elseif strcmp(action{trial,iterN},'right')
+					
+					switch extraReward{trial}
+						case 'left'
+							reward = 1 + rho;
+						case 'right'
+							reward = 1;
+						case 'none'
+							reward = 1;
+					end
+					
+				end
+				
+			else
+				
+				reward = 0 ;
+				
+			end
+			
+		else
+			
+			reward = 0 ;
+			
+		end
+		
+		
+		% calculate delta, and update Q values
+		if strcmp(action{trial,iterN},'left')
+			
+			delta(trial, iter)   = reward - QL(trial,iter);
+			
+			QLL     = QLL + alpha * delta(trial,iter) * Belief_L ;
+			QRL     = QRL + alpha * delta(trial,iter) * Belief_R ;
+			
+		else   % right
+			
+			delta(trial, iter)   = reward - QR(trial,iter);
+			
+			QLR     = QLR + alpha * delta(trial,iter) * Belief_L;
+			QRR     = QRR + alpha * delta(trial,iter) * Belief_R;
+			
+		end
+		
+	end
+	
 end
 
+actionLeft = strcmp(action,'left');
+actionRight = strcmp(action,'right');
+meanActionNum = mean(actionRight-actionLeft,2);
+
 % set output
-output = [mean(action,2), mean(QL,2), mean(QR,2)];
+output.action = meanActionNum;
+output.QL			= mean(QL,2);
+output.QL			= mean(QR,2);
 
 
 end
