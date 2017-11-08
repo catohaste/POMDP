81e341c9360fae97ff32bc678ceed52200dfa61c
heykayhay <heykayhay@users.noreply.github.com>
2017-11-08 10:55:14 +0000

    Change input data into a struct.

--- Main.m
+++ Main.m
@@ -9,71 +9,77 @@
 
 close all
 
-%% SET PARAMETER VALUES
+%% SET MODEL PARAMETER VALUES
 
 % set model parameter values
 alpha       = 0.35;     % learning rate
-rho         = 7.0;      % value of additional reward
-noiseSTD    = 0.14;     % noise in belief
+rho         = 4.0;      % value of additional reward
+noiseSTD    = 0.18;     % noise in belief
 
 params = [alpha rho noiseSTD];
 
-%% GENERATE DATA
 
-trialN = 2000;                      % number of trials
-blockN = 10;
-input = nan(trialN,2);              % create input array
+%% SET TASK PARAMETER VALUES
 
-% COLUMN 1 , CONTRAST
+trialN			= 2000;													% number of trials
+blockN			= 10;														% number of blocks
+extraReward = {'right','left','none'};			% options for extra reward side
+stimulus = ...
+	[-0.5 -0.2 -0.1 -0.05 0.05 0.1 0.2 0.5];	% possible stimulus values
 
-stimulus = [-0.5 -0.2 -0.1 -0.05 0.05 0.1 0.2 0.5];
-stimTrials = nan(trialN,1);
 
+%% GENERATE INPUT DATA
+
+% create input struct
+input	= struct;
+
+
+% initialise array to hold stimulus values for each trial
+input.stimTrials = zeros(trialN,1);
 
 for i=1:trialN
-   % here, a 'for' loop is used to ensure a different random number is
-   % generated on each iteration
-
-   % stimTrials(i) = (ceil(rand*26)-13.5)*0.04;
-   stimTrials(i) = stimulus(unidrnd(length(stimulus)));
-
+	% here, a 'for' loop is used to ensure a different random number is
+	% generated on each iteration
+	
+	% input.stimTrials(i) = (ceil(rand*26)-13.5)*0.04;
+	input.stimTrials(i) = stimulus(unidrnd(length(stimulus)));
+	
 end
 
-% set stimulus
-input(:,1) = stimTrials(:);
-
-
-% COLUMN 2 , REWARD BLOCK ID
-
+% choose lengths of each block
 midBlockLen = trialN/blockN;
 minBlockLen = 0.75*midBlockLen;
 maxBlockLen = 1.25*midBlockLen;
 rangeBlockLen = minBlockLen:maxBlockLen;
 blockLen = randsample(rangeBlockLen,blockN-1,true);
 
+% ensure block lengths add up to total trial number
 if sum(blockLen)<trialN
-   blockLen = [blockLen trialN-sum(blockLen)];
+	blockLen = [blockLen trialN-sum(blockLen)];
 else
-   blockLen(9) = trialN - sum(blockLen(1:8));
+	blockLen(9) = trialN - sum(blockLen(1:8));
 end
 
+% calculate cumulative sum of block lengths
 blockLenCumul = cumsum(blockLen);
 
-oddBlockTrials = nan(trialN,1);
 
-for i=1:trialN
-   
-   oddBlockTrials(i) = mod(sum(i > blockLenCumul) + 1,2);
-   
+% initialize cell array for extraRewardTrials
+input.extraRewardTrials = cell(trialN,1);
+
+% randomly choose extra reward side for each block
+blockID = cell(blockN,1);
+blockID(1) = randsample(extraReward,1,true);
+for i=2:blockN
+	blockID(i) = randsample(setdiff(extraReward,blockID{i-1}),1,true);
 end
 
-evenBlockTrials = 1 - oddBlockTrials;
-
-tmp = randsample([1 2],2);
-oddBlockID = tmp(1);
-evenBlockID = tmp(2);
-
-input(:,2) = oddBlockID*oddBlockTrials + evenBlockID*evenBlockTrials;
+% fill in correct reward for each trial
+input.extraRewardTrials(1:blockLenCumul(1)) = blockID(1);
+for i=2:blockN
+	input.extraRewardTrials(blockLenCumul(i-1):blockLenCumul(i)) ...
+		= blockID(i);
+end
 
 
 %% RUN
